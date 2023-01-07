20f19368dfe8a2f1747338f91ac82cb5fb5a329b
Roman Rodin <romaro83@gmail.com>
2023-01-08 01:32:26 +0300

    Add ApiCommands class

---
+++ Gui.Desktop/ApiCommands.cs
@@ -0,0 +1,17 @@
+﻿namespace Gui.Desktop
+{
+    internal static class ApiCommands
+    {
+        private static readonly string ApiSchemaName = "api_admin";
+
+        public static string CreateEntity(string publicName, string pascalName, bool isDocument)
+        {
+            return $"CALL {ApiSchemaName}.create_entity({publicName}, {pascalName}, {isDocument});";
+        }
+
+        public static string RemoveEntity(int entityId)
+        {
+            return $"CALL {ApiSchemaName}.remove_entity({entityId});";
+        }
+    }
+}
